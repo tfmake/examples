b13719618e6cd46e63dca4d659a1da8fe4bf3013
José Hermosilla <jrhermosilla.dev@gmail.com>
2023-03-02 00:25:26 +0100

    chore: add terraform files and workflows

---
+++ terraform/C/C2/main.tf
@@ -0,0 +1,20 @@
+locals {
+  content = "C2 module"
+}
+
+resource "local_file" "file" {
+  content  = local.content
+  filename = "${path.module}/out.txt"
+}
+
+resource "random_id" "id" {
+  keepers = {
+    content = local.content
+  }
+
+  byte_length = 8
+}
+
+output "id" {
+  value = random_id.id.id
+}
